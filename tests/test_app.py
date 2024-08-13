38a364fadd8d51b2ed729960ee2acc58cd747ed3
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-13 15:58:54 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
